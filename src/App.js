3a09a2524fa42b047789ebf3a750df726dc6348b
Ioan Zicu <ioan.zicu94@gmail.com>
2019-03-31 16:58:33 +0200

    Add the - Create Contact Button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    screen: 'create', //list, create
+    screen: 'list', //list, create
     contacts: []
   }
   removeContact = (contact) => {
@@ -27,6 +27,9 @@ class App extends Component {
           <ListContacts
             contacts={this.state.contacts}
             onDeleteContact={this.removeContact}
+            onNavigate={() => {
+              this.setState({ screen: 'create' })
+            }}
           />
         )}
         {this.state.screen === 'create' && (
